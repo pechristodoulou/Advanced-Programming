cd15a6b256064881732fb91ffdb03cbe9ddf8f8a
MyLaptop <pchristodoulou@science.ru.nl>
2025-06-27 23:29:42 +0200

    Added answer for item (g)

--- ex6.3/main.cc
+++ ex6.3/main.cc
@@ -5,6 +5,11 @@ using namespace std;
 Why are we doing this?
 → To make Stack generic and use the smart memory management from our Array<T>.
 → This reduces code duplication and increases flexibility.
+
+g) Do we need the default value in Stack?
+In Stack, elements are only added via push(), and we never access or rely on uninitialised positions.
+Because of this, the default value mechanism of Array<T> is not meaningful in context and therefore, 
+we should remove it to simplify the design.
 */
 
 int main() {
